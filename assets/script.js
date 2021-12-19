edd45f4157dc44af967193e1de7aa32fc0b0be97
sper0054 <teresa.sperl@gmail.com>
2021-12-19 15:23:26 -0600

    html and css

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,156 @@
+//Questions and Answers DONE
+var quiz= [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: ["strings", "booleans", "numbers", "alerts"],
+        answer: "alerts",
+    },
+    {
+        question: "The condition in an if/else statement is encolsed within ________.",
+        answers: ["quotes", "parentheses", "square brackets", "curly brackets"],
+        answer: "parentheses",
+    }
+    {
+        question: "Arrays in JavaScript can be used to store",
+        answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above",
+    }
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables",
+        answers: ["curly brackets", "commas", "quotes", "parantheses"],
+        answer: "quotes",
+    }
+    {
+        question: "A very useful tool used during development and debugging for print content to the debugger is",
+        answers: ["javascript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log",
+    }
+]
 
-//start the quiz by clicking the button
 
 
-//end the quiz
+/**set the time to zero*/
+var timer = 76;
+var timeCount;
+/**this is the timer funtion which will start counting as soon as the quiz starts*/
+function setupTimer() {
+    timeCount = setInterval(function () {
+        timer--;
+        var timeReset = timeElement.textContent = "Time:" + " " + timer;
+       timer = timer;
+        if (timer <= 0) {         
+            clearInterval(timeCount);
+              
+            timeElement.textContent = timeReset;
+             
+        }
+    }, 1000)
+}
+ 
+/**  Here is the event listener to start the timer and hide the quiz button*/
+document.addEventListener("click", function (event) {
+    if (event.target === btnElement) {
+        wrapperElement.style.display = "none";
+        setupTimer()
+        displayQuestions();
+    }
 
-// save initals and score to local storage
-var initalsInput = document.querySelector("initials");
-var scoreInput = document.querySelector("score");
+})
 
-signupButton.addEventListener("click", function(event) {
+ 
+/**declare the index variable for the onclickHandler function**/
+var i = 0;
+
+/**Add a function to compare the answers and 
+ * display each questions as the buttons are clicked.*/
+function onclickHandler(event) {
+     
+    if(timer<=0){
+        clearInterval(timeCount);
+        divContEL.style.display="none";
+        displayResult();
+    }
+    var answerText = event.target.textContent 
+    if (answerText === questions[i].answer) {
+        timer = timer;
+        responsDiv.setAttribute("style", "color: green")
+        responsDiv.textContent = "Correct";
+    } else {
+
+        responsDiv.setAttribute("style", "color: red")
+        responsDiv.textContent = "Wrong";
+        timer = timer - 15;
+     }
+    
+      
+     
+    if (i < questions.length-1) {
+
+      i++;
+
+      setTimeout(function () {
+      displayQuestions();
+      responsDiv.textContent = "";
+    }, 1000)
+    }else {
+        setTimeout(function () {
+            responsDiv.textContent = "";
+            displayResult();
+            clearInterval(timeCount);
+          
+        }, 500)
+    
+
+        divContEL.innerHTML = '';
+     }
+     
+    /**Function to display users final score */
+    function displayResult() {
+        finishDiv.style.visibility = "visible";
+        timeElement.textContent = "Time:" + " " + timer;
+        var HighScores = timer;
+        localStorage.getItem(HighScores)
+        finalScore.textContent = "Your finally score is: " + HighScores;
+         localStorage.setItem("HighScores", HighScores)
+ 
+    }
+}
+/**function to show the last page  */
+function renderLastItem() {
+    var yourScore = localStorage.getItem("HighScores");
+     var yourInitial = localStorage.getItem("Initial");
+     if (yourScore && yourInitial === "") {
+        return
+    }
+    finishDiv.textContent = "";
+    var finaPageEl = document.querySelector(".final-page");
+    finaPageEl.style.visibility = "visible";
+    var initialAndScore = document.querySelector("#staticEmail");
+    initialAndScore.value = yourInitial + ":" + " " + yourScore;
+
+}
+ 
+//** This event listner submit the initial and final score to the local storage */
+document.addEventListener("submit", function (event) {
     event.preventDefault();
+    var initialInput = document.querySelector("#inputInitial").value;
+    if (initialInput === "") {
+        errMsg.setAttribute("style", "color: red")
+        errMsg.textContent = "Initial input field cannot be empty"
+    } else {
+        errMsg.textContent = "";
+        localStorage.getItem(initialInput)
+        localStorage.setItem("Initial", initialInput)
+         renderLastItem()
+    }
 
-    var user = {
-        player: initialsInput.value.trim(),
-        score: scoreInput.value.trim(),
-    };
-
-    localStorage.setItem("user", JSON.stringify(user));
-});
+})
+/**This function will refresh the page and send user back to begining page when go back button is clicked */
+function init() {
+     location.reload();
+ 
+}
+/**This function will  clear initial and score displayed on the final page */
+function clearScore() {
+    initialAndScore.value = "";
+}
